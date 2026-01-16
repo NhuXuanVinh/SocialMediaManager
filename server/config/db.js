72058f2d24b5b78d4caa996850fd083b6f0512e7
NhuXuanVinh <vinhnhu30@gmail.com>
2026-01-17 04:20:25 +0700

    add

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,23 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
-const sequelize = new Sequelize({
-  host: process.env.DB_HOST || 'localhost',
-  dialect: 'postgres',
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT || 5432,
-  logging: false,
-});
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      logging: false,
+      dialectOptions: {
+        ssl: { require: true, rejectUnauthorized: false },
+      },
+    })
+  : new Sequelize({
+      host: process.env.DB_HOST,
+      dialect: "postgres",
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      port: process.env.DB_PORT || 5432,
+      logging: false,
+    });
 
 const connectDB = async () => {
   try {

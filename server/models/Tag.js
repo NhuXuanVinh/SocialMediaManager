55d2eba91c15904f7cd0de923340c27b72f4ac98
NhuXuanVinh <vinhnhu30@gmail.com>
2026-02-01 07:40:49 +0700

    fix tag name

--- server/models/Tag.js
+++ server/models/Tag.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true,
+        unique: false,
       },
       workspace_id: {
         type: DataTypes.BIGINT,
